29f8c26cf9dddbbed70e7ec13c8392541c3fe5ae
George Suveti <george.suveti@irian.ro>
2019-06-06 16:07:03 +0300

    - remove unused services - use h2 as db for shop resource server

--- spring-zuul/src/main/java/com/example/springzuul/SecurityConfiguration.java
+++ spring-zuul/src/main/java/com/example/springzuul/SecurityConfiguration.java
@@ -5,16 +5,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
-import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
 
 @Configuration
 @EnableResourceServer
 public class SecurityConfiguration extends ResourceServerConfigurerAdapter {
-
-    @Override
-    public void configure(final ResourceServerSecurityConfigurer resources) {
-        resources.tokenExtractor(new GrafanaCookieTokenExtractor());
-    }
 
 
     @Override
@@ -26,10 +20,7 @@ public class SecurityConfiguration extends ResourceServerConfigurerAdapter {
                 antMatchers("/authorization/status").
                 authenticated().
                 antMatchers("/blog/**").permitAll().
-                antMatchers("/shop/**").permitAll().
-                antMatchers("/grafana/public/**").permitAll().
-//                antMatchers("/grafana/**").authenticated();
-                antMatchers("/grafana/**").permitAll();
+                antMatchers("/shop/**").permitAll();
     }
 
 }
